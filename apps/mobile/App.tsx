d8dd96844986d30eff8183594c7cae094c71e835
zxTomw <tom_zhixing@outlook.com>
2025-05-28 02:27:37 -0400

    add bottom tabs

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -13,17 +13,72 @@ function HomeScreen() {
   );
 }
 
-const RootStack = createNativeStackNavigator({
+function FavoritesScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Favorites Screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function FridgeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Fridge Screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Profile Screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const RootTab = createBottomTabNavigator({
   initialRouteName: "Home",
   screens: {
     Home: {
       screen: HomeScreen,
-      options: { title: "想做的菜\，一键买齐", headerTitleAlign: "left" },
+      options: {
+        title: "首页",
+        headerTitleAlign: "left",
+        headerTitle: "想做的菜\，一键买齐",
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      options: {
+        title: "收藏",
+        headerTitleAlign: "left",
+        headerTitle: "收藏",
+      },
+    },
+    Fridge: {
+      screen: FridgeScreen,
+      options: {
+        title: "冰箱",
+        headerTitleAlign: "left",
+        headerTitle: "冰箱",
+      },
+    },
+    Profile: {
+      screen: ProfileScreen,
+      options: {
+        title: "我的",
+        headerTitleAlign: "left",
+        headerTitle: "我的",
+      },
     },
   },
 });
 
-const Navigation = createStaticNavigation(RootStack);
+const Navigation = createStaticNavigation(RootTab);
 
 export default function App() {
   return <Navigation />;
