8cdfb18793d46ac8867d1a2e143c9e053b3cb230
zxTomw <tom_zhixing@outlook.com>
2025-05-28 02:01:57 -0400

    migrate to bun

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,13 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createStaticNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
+function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+}
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: "Home",
+  screens: {
+    Home: {
+      screen: HomeScreen,
+      options: { title: "想做的菜\，一键买齐", headerTitleAlign: "left" },
+    },
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
 }
 
 const styles = StyleSheet.create({
